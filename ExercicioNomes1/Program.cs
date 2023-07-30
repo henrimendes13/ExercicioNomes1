cc01274cc78c2c9f5568fbc34d936cd1a9c61dbc
Henrique Mendes <henriquemendes13@gmail.com>
2023-07-30 20:50:36 -0300

    TreinandoSplit

--- ExercicioNomes1/Program.cs
+++ ExercicioNomes1/Program.cs
@@ -1,16 +1,22 @@
-﻿string nome1, nome2; 
-double idade1, idade2, idadeMedia;
+﻿ string nome1, nome2;
+        double idade1, idade2, idadeMedia;
+
+        string[] vet;
+
+        Console.WriteLine("Nome e idade: ");
+        vet = Console.ReadLine().Split(' ');
+        nome1 = vet[0];
+        idade1 = int.Parse(vet[1]);
 
 
-Console.WriteLine("Nome 1: ");
-nome1 = Console.ReadLine();
-Console.WriteLine("Idade: ");
-idade1 = double.Parse(Console.ReadLine());
+        string[] vet2;
 
-Console.WriteLine("Nome 2: ");
-nome2 = Console.ReadLine();
-Console.WriteLine("Idade: ");
-idade2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Nome e idade: ");
+        vet2 = Console.ReadLine().Split(' ');
+        nome2 = vet2[0];
+        idade2 = int.Parse(vet2[1]);
 
-idadeMedia = (idade1 + idade2) / 2;
-Console.WriteLine("A media de idade é: " + idadeMedia.ToString("F1"));
+
+        idadeMedia = (idade1 + idade2) / 2;
+
+        Console.WriteLine("A media de idade é: " + idadeMedia.ToString("F1"));
